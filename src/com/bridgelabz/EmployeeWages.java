b6b89735bef8c4ad07fbe49aef7b5608b2b92c9a
Yogesh <mishra92yogesh@gmail.com>
2022-09-22 23:16:32 +0530

    Program to calculate daily wages

--- src/com/bridgelabz/EmployeeWages.java
+++ src/com/bridgelabz/EmployeeWages.java
@@ -2,10 +2,15 @@ package com.bridgelabz;
 
 public class EmployeeWages {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wages program.");
+        System.out.println("Program to calculate daily wages.");
+        int ratePerHour=20;
+        int workingHour=8,dailyWages;
         int checkAttendance= (int) Math.floor(Math.random()*10)%2;
         if(checkAttendance==0)
-            System.out.println("Employee is present.");
+        {
+            dailyWages=ratePerHour*workingHour;
+            System.out.println("Daily wages of employee: " + dailyWages);
+        }
         else
             System.out.println("Employee is absent");
     }
