b42528866c58a5e0acc63f6001a1767d73ee6631
Yogesh <mishra92yogesh@gmail.com>
2022-09-22 23:38:58 +0530

    Program to calculate wages using switch

--- src/com/bridgelabz/EmployeeWages.java
+++ src/com/bridgelabz/EmployeeWages.java
@@ -2,16 +2,22 @@ package com.bridgelabz;
 
 public class EmployeeWages {
     public static void main(String[] args) {
-        System.out.println("Program to add part time employee.");
+        System.out.println("Program to calculate employee wages using switch cases.");
         int ratePerHour=20;
-        int partTimeHour=4,dailyWages;
-        int checkAttendance= (int) Math.floor(Math.random()*10)%2;
-        if(checkAttendance==0)
+        int partTimeHour=4,fullTimeHour=8,dailyWages;
+        int checkAttendance= (int) Math.floor(Math.random()*10)%3;
+        switch(checkAttendance)
         {
-            dailyWages=ratePerHour*partTimeHour;
-            System.out.println("Daily wages of part time employee: " + dailyWages);
+            case 1:
+                dailyWages=ratePerHour*fullTimeHour;
+                System.out.println("Daily wages of full time employee: " + dailyWages);
+                break;
+            case 2:
+                dailyWages=ratePerHour*partTimeHour;
+                System.out.println("Daily wages of part time employee: " + dailyWages);
+                break;
+            default:
+                System.out.println("Employee is absent.");
         }
-        else
-            System.out.println("Employee is absent");
     }
 }
