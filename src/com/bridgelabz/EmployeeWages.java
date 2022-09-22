cf82e5913c2d286c345b46093368e09dd419ed6d
Yogesh <mishra92yogesh@gmail.com>
2022-09-22 23:25:37 +0530

    Program to add part time employee

--- src/com/bridgelabz/EmployeeWages.java
+++ src/com/bridgelabz/EmployeeWages.java
@@ -2,14 +2,14 @@ package com.bridgelabz;
 
 public class EmployeeWages {
     public static void main(String[] args) {
-        System.out.println("Program to calculate daily wages.");
+        System.out.println("Program to add part time employee.");
         int ratePerHour=20;
-        int workingHour=8,dailyWages;
+        int partTimeHour=4,dailyWages;
         int checkAttendance= (int) Math.floor(Math.random()*10)%2;
         if(checkAttendance==0)
         {
-            dailyWages=ratePerHour*workingHour;
-            System.out.println("Daily wages of employee: " + dailyWages);
+            dailyWages=ratePerHour*partTimeHour;
+            System.out.println("Daily wages of part time employee: " + dailyWages);
         }
         else
             System.out.println("Employee is absent");
