be74ea5078c657f7e90f121f736832a4d67f220e
Yogesh <mishra92yogesh@gmail.com>
2022-09-22 23:45:40 +0530

    Program to calculate monthly wages

--- src/com/bridgelabz/EmployeeWages.java
+++ src/com/bridgelabz/EmployeeWages.java
@@ -2,19 +2,19 @@ package com.bridgelabz;
 
 public class EmployeeWages {
     public static void main(String[] args) {
-        System.out.println("Program to calculate employee wages using switch cases.");
+        System.out.println("Program to calculate monthly wages of employees.");
         int ratePerHour=20;
-        int partTimeHour=4,fullTimeHour=8,dailyWages;
+        int partTimeHour=4,fullTimeHour=8,totalWorkingDay=30, monthlyWages;
         int checkAttendance= (int) Math.floor(Math.random()*10)%3;
         switch(checkAttendance)
         {
             case 1:
-                dailyWages=ratePerHour*fullTimeHour;
-                System.out.println("Daily wages of full time employee: " + dailyWages);
+                monthlyWages=ratePerHour*fullTimeHour*totalWorkingDay;
+                System.out.println("Daily wages of full time employee: " + monthlyWages);
                 break;
             case 2:
-                dailyWages=ratePerHour*partTimeHour;
-                System.out.println("Daily wages of part time employee: " + dailyWages);
+                monthlyWages=ratePerHour*partTimeHour*totalWorkingDay;
+                System.out.println("Daily wages of part time employee: " + monthlyWages);
                 break;
             default:
                 System.out.println("Employee is absent.");
